10d88ce6183f99591899b0c5af6e7b0ef9f99cc4
JlwxdQueen <148983664+JlwxdQueen@users.noreply.github.com>
2025-01-13 20:21:09 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Algoth/extension_processer
-# Build directory: D:/Algoth/extension_processer/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ProcessorTests "D:/Algoth/extension_processer/cmake-build-debug/ProcessorTests.exe")
-set_tests_properties(ProcessorTests PROPERTIES  _BACKTRACE_TRIPLES "D:/Algoth/extension_processer/CMakeLists.txt;40;add_test;D:/Algoth/extension_processer/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
